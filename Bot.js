8fb771bd0505d3d57fa8cdc251159f9809f11e8a
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-11-28 07:41:25 +1100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -15,9 +15,6 @@ const bot = mineflayer.createBot({
   port: config.port,
   version: config.version
   
-  //port: '25569',                // only set if you need a port that isn't 25565
-  // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
-  // auth: 'mojang'              // only set if you need microsoft auth, then set this to 'microsoft'
 })
 
 
