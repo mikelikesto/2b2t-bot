46342d621c156fd03302dc2e27a68f92cb963fd6
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-11-29 20:47:10 +1100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -54,7 +54,7 @@ bot.on('chat', (username, message, prefix) => {
 bot.on('chat', (username, message, prefix) => {
 
   function intervalFunc() {
-    bot.chat(message.slice(4) + '/tp');
+    bot.chat('/tp' + message.slice(4) );
   }
   if  (message.startsWith( config.prefix + 'tp')) {
     setInterval(intervalFunc, config.speed) 
